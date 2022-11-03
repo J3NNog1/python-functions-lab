96750feadab152feca3d2b3802dc8a72d4dc5bb4
Jenn <jenniferjuarezjj@gmail.com>
2022-11-03 08:32:30 -0400

    part 2 done

--- python_functions.py
+++ python_functions.py
@@ -8,3 +8,20 @@ def sum_to(num):
   for n in range(1, num + 1):
     sum += n
   return sum
+
+#part 2
+
+largest([1, 2, 3, 4, 0])  # returns 4
+largest([10, 4, 2, 231, 91, 54])  # returns 231
+
+def largest(nums):
+  largest = 0
+  for num in nums:
+    if num > largest:
+      largest = num
+  return largest
+  
+# Sort the list approach
+def largest(nums):
+  nums.sort()
+  return nums[-1]
